f48dfbec8d66177efd4a3c5fd01aae3ebb522409
bealira <8190124@estg.ipp.pt>
2020-06-12 22:03:00 +0100

    First class: Position

--- src/chess/Chess.java
+++ src/chess/Chess.java
@@ -5,6 +5,8 @@
  */
 package chess;
 
+import boardGame.Position;
+
 /**
  *
  * @author Bea
@@ -17,6 +19,9 @@ public class Chess {
     public static void main(String[] args) {
         
         System.out.println("Welcome to the chess game!");
+        
+        Position position=new Position(3, 5);
+        System.out.println(position.toString());
         
     }
     
