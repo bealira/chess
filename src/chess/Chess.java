d1f5b3ab365144c19d2f6687bc5cba75070817f6
bealira <8190124@estg.ipp.pt>
2020-06-13 20:28:25 +0100

    Switching player each turn

--- src/chess/Chess.java
+++ src/chess/Chess.java
@@ -31,7 +31,7 @@ public class Chess {
         while(true){
             try{
                 UI.clearScreen();
-                UI.printBoard(chessMatch.getPieces());
+                UI.printMatch(chessMatch);
                 System.out.println();
                 System.out.print("Source: ");
                 ChessPosition source=UI.readChessPosition(sc);
