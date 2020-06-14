aef5383214b610744f8d7dfea741a376b1df9fe6
bealira <8190124@estg.ipp.pt>
2020-06-14 12:09:44 +0100

    Checkmate logic

--- src/chess/Chess.java
+++ src/chess/Chess.java
@@ -31,7 +31,7 @@ public class Chess {
         ChessMatch chessMatch=new ChessMatch();
         List<ChessPiece> captured=new ArrayList<>();
         
-        while(true){
+        while(!chessMatch.getCheckMate()){
             try{
                 UI.clearScreen();
                 UI.printMatch(chessMatch,captured);
@@ -62,7 +62,8 @@ public class Chess {
             }
         }
         
-        
+        UI.clearScreen();
+        UI.printMatch(chessMatch, captured);
         
     }
     
